512cd9ef3a83831e2040f477f60b94f65737c4c4
Brams <brams_s@icloud.com>
2023-11-15 20:13:13 -0400

    style(ui): Making dark mode styling more clear

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -13,11 +13,8 @@ export const runtime = "edge";
 
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <div>
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-pink-400">T3</span> Turbo
-        </h1>
         <AuthShowcase />
 
         <CreatePostForm />
@@ -26,8 +23,8 @@ export default function HomePage() {
             fallback={
               <div className="flex w-full flex-col gap-4">
                 <PostCardSkeleton />
-                <PostCardSkeleton />
-                <PostCardSkeleton />
+                {/* <PostCardSkeleton /> */}
+                {/* <PostCardSkeleton /> */}
               </div>
             }
           >
@@ -36,7 +33,9 @@ export default function HomePage() {
         </div>
       </div>
 
-      <ThemeSwitch />
-    </main>
+      <div className="">
+        <ThemeSwitch />
+      </div>
+    </div>
   );
 }
